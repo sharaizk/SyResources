5932325aaab041e19ea292979d13e2b57bc5e23a
Sharaiz Khan <sharaizkhan78@gmail.com>
2022-08-23 22:44:10 +0500

    Image - compressed

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -22,7 +22,7 @@ import * as Yup from "yup";
 import { useTheme } from "styled-components";
 import { GrMapLocation } from "react-icons/gr";
 import { MdCall, MdOutlineEmail } from "react-icons/md";
-
+import { Fade } from "react-reveal";
 const Contact = () => {
   const form = useRef(null);
   const theme = useTheme();
@@ -79,115 +79,119 @@ const Contact = () => {
   return (
     <ScreenContainer>
       <BreadCrumb />
-      <TopContainer>
-        <Title>Contact Us</Title>
-        <p>You can contact us through following means:</p>
-        <br />
-        <CardDescriptio>
-          <Title2>
-            <GrMapLocation size={24} color={theme.primary} /> Address:
-          </Title2>
-          <Description2>
-            Kilroy House, 400 Kilroy House, 400, Roding Lane South, Woodford
-            Green, England, IG8 8EY
-          </Description2>
-        </CardDescriptio>
-        <br />
-        <CardDescriptio>
-          <Title2>
-            <MdOutlineEmail size={24} /> Email:
-          </Title2>
-          <Description2>
-            <a href="mailto:syresourcesltd@gmail.com">
-              syresourcesltd@gmail.com
-            </a>
-          </Description2>
-        </CardDescriptio>
-        <br />
-        <CardDescriptio>
-          <Title2>
-            <MdCall size={24} /> Phone:
-          </Title2>
-          <Description2>07576503089</Description2>
-        </CardDescriptio>
-        <br />
-        <p>OR</p>
-        <br />
+      <Fade left>
+        <TopContainer>
+          <Title>Contact Us</Title>
+          <p>You can contact us through following means:</p>
+          <br />
+          <CardDescriptio>
+            <Title2>
+              <GrMapLocation size={24} color={theme.primary} /> Address:
+            </Title2>
+            <Description2>
+              Kilroy House, 400 Kilroy House, 400, Roding Lane South, Woodford
+              Green, England, IG8 8EY
+            </Description2>
+          </CardDescriptio>
+          <br />
+          <CardDescriptio>
+            <Title2>
+              <MdOutlineEmail size={24} /> Email:
+            </Title2>
+            <Description2>
+              <a href="mailto:syresourcesltd@gmail.com">
+                syresourcesltd@gmail.com
+              </a>
+            </Description2>
+          </CardDescriptio>
+          <br />
+          <CardDescriptio>
+            <Title2>
+              <MdCall size={24} /> Phone:
+            </Title2>
+            <Description2>07576503089</Description2>
+          </CardDescriptio>
+          <br />
+          <p>OR</p>
+          <br />
 
-        <p className="bold">
-          <b>
-            If you want to speak to us over the phone, please leave your details
-            and one of our experts will get in touch with you.
-          </b>
-        </p>
-      </TopContainer>
-      <ContactContainer>
-        <FormikProvider value={formik}>
-          <StyledForm
-            autoComplete="off"
-            noValidate
-            onSubmit={handleSubmit}
-            ref={form}
-          >
-            <InputLabel>Full Name:</InputLabel>
-            <Input
-              type="text"
-              id="fullName"
-              name="fullName"
-              value={values.fullName}
-              {...getFieldProps("fullName")}
-            />
-            {touched.fullName && errors.fullName && (
-              <Error>{errors?.fullName}</Error>
-            )}
-            <InputLabel>Email:</InputLabel>
-            <Input
-              type="email"
-              id="email"
-              name="email"
-              value={values.email}
-              {...getFieldProps("email")}
-            />
-            {touched.email && errors.email && <Error>{errors?.email}</Error>}
-            <InputLabel>Telephone:</InputLabel>
-            <Input
-              type="number"
-              id="telephone"
-              name="telephone"
-              value={values.telephone}
-              {...getFieldProps("telephone")}
-            />
-            {touched.telephone && errors.telephone && (
-              <Error>{errors?.telephone}</Error>
-            )}
-            <InputLabel>Subject:</InputLabel>
-            <Input
-              type="text"
-              id="subject"
-              name="subject"
-              value={values.subject}
-              {...getFieldProps("subject")}
-            />
-            {touched.subject && errors.subject && (
-              <Error>{errors?.subject}</Error>
-            )}
-            <InputLabel>Message:</InputLabel>
-            <TextArea
-              rows="4"
-              cols="50"
-              type="text"
-              id="message"
-              name="message"
-              value={values.message}
-              {...getFieldProps("message")}
-            />
-            {touched.message && errors.message && (
-              <Error>{errors?.message}</Error>
-            )}
-            <Submit $disabled={isSubmitting}>Send Message</Submit>
-          </StyledForm>
-        </FormikProvider>
-      </ContactContainer>
+          <p className="bold">
+            <b>
+              If you want to speak to us over the phone, please leave your
+              details and one of our experts will get in touch with you.
+            </b>
+          </p>
+        </TopContainer>
+      </Fade>
+      <Fade left>
+        <ContactContainer>
+          <FormikProvider value={formik}>
+            <StyledForm
+              autoComplete="off"
+              noValidate
+              onSubmit={handleSubmit}
+              ref={form}
+            >
+              <InputLabel>Full Name:</InputLabel>
+              <Input
+                type="text"
+                id="fullName"
+                name="fullName"
+                value={values.fullName}
+                {...getFieldProps("fullName")}
+              />
+              {touched.fullName && errors.fullName && (
+                <Error>{errors?.fullName}</Error>
+              )}
+              <InputLabel>Email:</InputLabel>
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                value={values.email}
+                {...getFieldProps("email")}
+              />
+              {touched.email && errors.email && <Error>{errors?.email}</Error>}
+              <InputLabel>Telephone:</InputLabel>
+              <Input
+                type="number"
+                id="telephone"
+                name="telephone"
+                value={values.telephone}
+                {...getFieldProps("telephone")}
+              />
+              {touched.telephone && errors.telephone && (
+                <Error>{errors?.telephone}</Error>
+              )}
+              <InputLabel>Subject:</InputLabel>
+              <Input
+                type="text"
+                id="subject"
+                name="subject"
+                value={values.subject}
+                {...getFieldProps("subject")}
+              />
+              {touched.subject && errors.subject && (
+                <Error>{errors?.subject}</Error>
+              )}
+              <InputLabel>Message:</InputLabel>
+              <TextArea
+                rows="4"
+                cols="50"
+                type="text"
+                id="message"
+                name="message"
+                value={values.message}
+                {...getFieldProps("message")}
+              />
+              {touched.message && errors.message && (
+                <Error>{errors?.message}</Error>
+              )}
+              <Submit $disabled={isSubmitting}>Send Message</Submit>
+            </StyledForm>
+          </FormikProvider>
+        </ContactContainer>
+      </Fade>
     </ScreenContainer>
   );
 };
